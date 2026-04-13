f17df657543a6758cb62fe402b2ca592b80c7b37
chinedu umeh <chineduumeh@chinedus-MacBook-Pro.local>
2026-04-13 11:03:33 +0100

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,63 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { tabs } from '@/constants/data'
+import { styles } from '@/constants/colors'
+import clsx from 'clsx'
+import {useSafeAreaInsets, SafeAreaView} from 'react-native-safe-area-context'
+import { components, colors } from '@/constants/theme'
+
+const tabBar = components.tabBar
+export default function TabsRootLayout() {
+    const insets = useSafeAreaInsets();
+    console.log(`this is the Top Value ${insets.top} and this is the bottom value ${insets.bottom}`)
+    console.log(`this is the horizontal value ${tabBar.horizontalInset} `)
+    const TabIcon = ({focused, icon}: TabIconProps) => {
+        const bg = focused ? 'bg-[#ea7a53]' : ''
+        return (
+           <View className='size-12 items-center justify-center'>
+            <View className={`size-12 items-center justify-center rounded-full bg-transparent ${bg}`}>
+                <Image source={icon} resizeMode='contain' className='size-6'/>
+            </View>
+           </View>
+        )
+    }
+  return (
+    <>
+     <Tabs 
+     screenOptions={{ 
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle:{
+            position: "absolute",
+            bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+            height: tabBar.height,
+            marginHorizontal: tabBar.horizontalInset,
+            borderRadius: tabBar.radius,
+            backgroundColor: colors.primary
+        },
+        tabBarItemStyle:{
+            paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6,
+        },
+       tabBarIconStyle:{
+        width: tabBar.iconFrame,
+        height: tabBar.iconFrame,
+        alignItems: "center",
+       }
+    }}
+     >
+     {tabs.map((tab) => (
+        <Tabs.Screen 
+        name={tab.name} 
+        key={tab.name} 
+        options={{ 
+            title: tab.title,
+            tabBarIcon: ({focused}) => (
+                <TabIcon focused={focused} icon={tab.icons}/>
+            )
+        }}/>
+     ))}
+    </Tabs>
+</>
+  )
+}
---
+++ constants/colors.ts
@@ -0,0 +1,8 @@
+export const styles = {
+  tabs: {
+    icon: "size-12 items-center justify-center",
+    active: "bg-accent",
+    pill: "size-12 items-center justify-center rounded-full bg-transparent",
+    glyph: ""
+  },
+};
